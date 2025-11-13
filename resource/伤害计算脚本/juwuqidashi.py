162cc083e1855f46f720c550c605d160327439bd
wang-xu-97 <wang-xu-97@users.noreply.github.com>
2025-11-13 01:57:07 +0000

    deploy: 9c7645554ff86045b1b2d7454d824452c77bd8fd

--- resource/伤害计算脚本/juwuqidashi.py
+++ resource/伤害计算脚本/juwuqidashi.py
@@ -10,13 +10,15 @@ coord_info = {
 
 D_p_normal = nncde(12)
 D_p_normal_buffed = nncde(12) + nncde(4) + nncde(4) + nncde(6)
+print(D_p_normal)
+print(D_p_normal_buffed)
 def f1(C, B):
     """关闭巨武器大师 均势伤害期望公式"""
     # return a**2 + b**2
     t = np.maximum(20-C-B, 0)
     hit_chance_special = (2 * (C + 1) + t) / 20
     hit_chance = ((C + 1) + t) / 20
-    return D_p_normal * hit_chance_special + 3 * hit_chance
+    return D_p_normal_buffed * hit_chance_special + 3 * hit_chance
 
 def f2(C, B):
     """打开巨武器大师 均势伤害期望公式"""
@@ -24,4 +26,4 @@ def f2(C, B):
     t = np.maximum(20-C-B-5, 0)
     hit_chance_special = (2 * (C + 1) + t) / 20
     hit_chance = ((C + 1) + t) / 20
-    return D_p_normal * hit_chance_special + 13 * hit_chance
+    return D_p_normal_buffed * hit_chance_special + 13 * hit_chance
