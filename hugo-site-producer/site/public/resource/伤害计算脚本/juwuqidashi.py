9c7645554ff86045b1b2d7454d824452c77bd8fd
wangxu <974779924@qq.com>
2025-11-13 09:56:49 +0800

    update

--- hugo-site-producer/site/public/resource/伤害计算脚本/juwuqidashi.py
+++ hugo-site-producer/site/public/resource/伤害计算脚本/juwuqidashi.py
@@ -16,7 +16,7 @@ def f1(C, B):
     t = np.maximum(20-C-B, 0)
     hit_chance_special = (2 * (C + 1) + t) / 20
     hit_chance = ((C + 1) + t) / 20
-    return D_p_normal * hit_chance_special + 3 * hit_chance
+    return D_p_normal_buffed * hit_chance_special + 3 * hit_chance
 
 def f2(C, B):
     """打开巨武器大师 均势伤害期望公式"""
@@ -24,4 +24,4 @@ def f2(C, B):
     t = np.maximum(20-C-B-5, 0)
     hit_chance_special = (2 * (C + 1) + t) / 20
     hit_chance = ((C + 1) + t) / 20
-    return D_p_normal * hit_chance_special + 13 * hit_chance
+    return D_p_normal_buffed * hit_chance_special + 13 * hit_chance
