f56554115d8f06f16ef384d0576406bc7e4cc4a0
wang-xu-97 <wang-xu-97@users.noreply.github.com>
2025-11-14 01:33:13 +0000

    deploy: d20cd68ef0f0e31ed0a6aad517639642d4f26353

--- resource/伤害计算脚本/tool.py
+++ resource/伤害计算脚本/tool.py
@@ -1,4 +1,5 @@
 import re
+import numpy as np
 
 def normal_non_crit_dice_expectation(d):
     return (1+d)/2
@@ -8,6 +9,45 @@ def xmds_non_crit_dice_expectation(d):
 
 nncde = normal_non_crit_dice_expectation
 xncde = xmds_non_crit_dice_expectation
+
+
+def factory(args):
+    
+    skill_status = {}
+    coord_info = {
+        'title': '凶蛮打手', 
+        'x': '重击减值', 
+        'y': '实际护甲Ac-Bonus', 
+        'z': '伤害期望'
+    }
+    dice_n = 1
+    dice_d = 12
+
+    D_p_normal = nncde(dice_d)
+    D_p_xmds = xncde(dice_d)
+    D_p_normal_buffed = nncde(12) + nncde(4) + nncde(4) + nncde(6)
+    D_p_xmds_buffed = xncde(12) + xncde(4) + xncde(4) + xncde(6)
+    print(D_p_normal)
+    print(D_p_xmds)
+    def f1(C, B):
+        """无凶蛮打手"""
+        # return a**2 + b**2
+        t = np.maximum(20-C-B, 0)
+        hit_chance_special = (2 * (C + 1) + t) / 20
+        hit_chance = ((C + 1) + t) / 20
+        return D_p_normal * hit_chance_special + 3 * hit_chance
+
+    def f2(C, B):
+        """有凶蛮打手"""
+        # return a * b
+        t = np.maximum(20-C-B, 0)
+        hit_chance_special = (2 * (C + 1) + t) / 20
+        hit_chance = ((C + 1) + t) / 20
+        return D_p_xmds_buffed * hit_chance_special + 3 * hit_chance
+
+
+    return f1, f2, coord_info
+
 
 def text_to_markdown_table(data_text):
     """
